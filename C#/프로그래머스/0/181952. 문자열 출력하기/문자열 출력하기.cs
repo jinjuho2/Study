1b4e4d03660dcfb4345b3cbeb8d5dbb064d2f9d6
jinjuho2 <jinjuho3@gmail.com>
2025-05-03 19:35:56 +0900

    [level 0] Title: 문자열 출력하기, Time: 17.52 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        Console.WriteLine(s);
+
+    }
+}
