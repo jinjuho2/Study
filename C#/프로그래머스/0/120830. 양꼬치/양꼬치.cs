4250be3d5abc1d67763c45a928c7077314fae308
jinjuho2 <jinjuho3@gmail.com>
2025-05-04 14:16:13 +0900

    [level 0] Title: 양꼬치, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        k -= n / 10;
+        answer = n * 12000 + k * 2000;
+        return answer;
+    }
+}
