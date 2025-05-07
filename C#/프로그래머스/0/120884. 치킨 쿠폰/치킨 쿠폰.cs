e62229c7b5d4214e4d1ca943e98d6714562f6117
jinjuho2 <jinjuho3@gmail.com>
2025-05-07 16:55:08 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = chicken;
+        
+        while (coupon >= 10)
+        {
+            int service = coupon / 10;
+            answer += service;
+            coupon = coupon % 10 + service;
+        }
+        
+        return answer;
+    }
+}
